36737ca9780cc216a8d9d6b4d3f7fe7530da08b7
dieratob <dieratob@students.zhaw.ch>
2025-02-26 08:37:00 +0100

    new mail

--- Start.py
+++ Start.py
@@ -8,8 +8,7 @@ st.title("Meine erste Streamlit App")
 # Streamlit über den Text unten direkt in die App - cool!
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Tobias Dierauer (dieratob@student.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
