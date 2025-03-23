234f64c6240e013290a8b8c34d5bf66c89b28a5e
Tobias Dierauer <dieratob@students.zhaw.ch>
2025-03-23 19:12:30 +0100

    Switch Drive

--- Start.py
+++ Start.py
@@ -3,7 +3,7 @@ from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
 
 # initialize the data manager
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_DB")  # switch drive 
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD-Inf2-AppV0-main")  # switch drive 
 
 # initialize the login manager
 login_manager = LoginManager(data_manager)
