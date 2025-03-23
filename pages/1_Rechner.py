967fa74970132f2a7073fdbddae6dd25f2f814e9
Tobias Dierauer <dieratob@students.zhaw.ch>
2025-03-23 18:37:35 +0100

    Login

--- pages/1_Rechner.py
+++ pages/1_Rechner.py
@@ -1,3 +1,11 @@
+# ====== Start Login Block ======
+from utils.login_manager import LoginManager
+LoginManager().go_to_login('Start.py') 
+# ====== End Login Block ======
+
+
+
+
 import streamlit as st
 from bmi_calculator import calculate_bmi
 
