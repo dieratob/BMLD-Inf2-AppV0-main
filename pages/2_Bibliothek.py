6fda154382e09ee2790623c1fa3467988d9f9d7e
Tobias Dierauer <dieratob@students.zhaw.ch>
2025-05-20 14:38:03 +0200

    Bugfix

--- pages/2_Bibliothek.py
+++ pages/2_Bibliothek.py
@@ -1,26 +1,13 @@
 import streamlit as st
-from hidden_pages.Begriff import BEGRIFFSINFOS
+from urllib.parse import quote
 
-# Entdeckte Begriffe laden
 entdeckte_begriffe = st.session_state.get("entdeckte", set())
-params = st.experimental_get_query_params()
-ausgewählter_begriff = params.get("name", [None])[0]
 
 st.title("📚 Hämatologie Bibliothek")
 
-# Begriffe anzeigen mit Button
 if entdeckte_begriffe:
     for begriff in sorted(entdeckte_begriffe):
-        if st.button(begriff):
-            st.experimental_set_query_params(name=begriff)
-            st.experimental_rerun()
+        link = f"/Detail?name={quote(begriff)}"
+        st.markdown(f"- [{begriff}]({link})")
 else:
     st.info("Noch keine Begriffe entdeckt.")
-
-# Detailansicht anzeigen
-if ausgewählter_begriff:
-    st.markdown("---")
-    st.subheader(f"🔍 Detailansicht: {ausgewählter_begriff}")
-    st.write(BEGRIFFSINFOS.get(ausgewählter_begriff, "Keine Infos vorhanden."))
-
-
